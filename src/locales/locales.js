a0d550920c617a8efc3530b3c604e9ad97198bcd
Pavel <p.yudenko@gmail.com>
2023-04-30 20:51:56 +0300

    refactor: split locales into modules

--- src/locales/locales.js
+++ src/locales/locales.js
@@ -1,46 +1,15 @@
 import ru from './ru';
 import en from './en';
+import servicesKeys from './servicesKeys';
+
+const { services, digits, shifted } = servicesKeys;
 
 const locales = {
   en,
   ru,
-  services: {
-    AltRight: 'alt',
-    ControlRight: 'ctrl',
-    Backspace: '',
-    Enter: '',
-    ShiftRight: '',
-    ArrowLeft: '',
-    ArrowRight: '',
-    ArrowUp: '',
-    ArrowDown: '',
-    CapsLock: 'caps lock',
-    ShiftLeft: '',
-    ControlLeft: 'ctrl',
-    MetaLeft: '',
-    AltLeft: 'alt',
-    Tab: '',
-    Delete: 'del',
-  },
-  digits: {
-    Digit1: '1',
-    Digit5: '5',
-    Digit8: '8',
-    Digit9: '9',
-    Digit0: '0',
-    Minus: '-',
-    Equal: '=',
-    Space: ' ',
-  },
-  shifted: {
-    Digit1: '!',
-    Digit5: '%',
-    Digit8: '*',
-    Digit9: '(',
-    Digit0: ')',
-    Minus: '_',
-    Equal: '+',
-  },
+  services,
+  digits,
+  shifted,
 };
 
 export default locales;
