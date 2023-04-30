9e5faf5ac0950d2075e967aa4ebce2f8afabab50
Pavel <p.yudenko@gmail.com>
2023-04-30 23:07:04 +0300

    refactor: rename files

--- src/handlesProcesses/handleCaretPosition.js
+++
@@ -1,9 +0,0 @@
-import getData from './utils/getData';
-
-const handleCaretPosition = (keyboard, state) => {
-  const textarea = keyboard.querySelector('.field');
-  const { splittedData } = getData(textarea);
-  state.carretPosition = (splittedData.length - 1) * 18;
-}
-
-export default handleCaretPosition;
